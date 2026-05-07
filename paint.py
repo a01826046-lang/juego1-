b9de804cccdf4ee81cad4dde69414050cf90a318
Mariana Tovar Vertiz <A01826046@tec.mx>
2026-05-07 13:47:43 -0600

    Added a new color for paint, draw a circle

--- paint.py
+++ paint.py
@@ -38,7 +38,17 @@ def square(start, end):
 
 def circle(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    radius = abs(end.x - start.x)
+
+    import turtle
+    turtle.circle(radius)
+
+    end_fill()
 
 
 def rectangle(start, end):
@@ -79,6 +89,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('purple'), 'P')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
